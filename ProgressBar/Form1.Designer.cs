442416ef0399043fd92a36bd40371d38d1f7ac22
Dima Senko <iwannaexplore@gmail.com>
2021-08-20 17:59:25 +0300

    Add project files.

---
+++ ProgressBar/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.readyBox = new System.Windows.Forms.TextBox();
+            this.neadedBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Subject = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(4, 86);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(235, 20);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Готово";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Надо";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(96, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0%";
+            // 
+            // readyBox
+            // 
+            this.readyBox.Location = new System.Drawing.Point(11, 43);
+            this.readyBox.Name = "readyBox";
+            this.readyBox.Size = new System.Drawing.Size(75, 20);
+            this.readyBox.TabIndex = 4;
+            // 
+            // neadedBox
+            // 
+            this.neadedBox.Location = new System.Drawing.Point(92, 43);
+            this.neadedBox.Name = "neadedBox";
+            this.neadedBox.Size = new System.Drawing.Size(75, 20);
+            this.neadedBox.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GetResult);
+            // 
+            // Subject
+            // 
+            this.Subject.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Subject.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Subject.Location = new System.Drawing.Point(38, 1);
+            this.Subject.Name = "Subject";
+            this.Subject.Size = new System.Drawing.Size(157, 23);
+            this.Subject.TabIndex = 7;
+            this.Subject.Text = "Предмет";
+            this.Subject.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(4, 165);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(235, 170);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "C# Pro - 10.05.2000 - 14/305 - 35%";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "История";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(249, 347);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Subject);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.neadedBox);
+            this.Controls.Add(this.readyBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Прогресс";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox readyBox;
+        private System.Windows.Forms.TextBox neadedBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Subject;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ProgressBar/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ReadFromFile();
+        }
+        private double  Calculate()
+        {
+            var neededTime = ReturnTimeFromString(neadedBox.Text);
+            var readyTime = ReturnTimeFromString(readyBox.Text);
+
+            double result = readyTime / neededTime * 100;
+            progressBar1.Value = (int)Math.Round(result);
+            label3.Text = $"{Math.Round(result)}%";
+            return result;
+        }
+        private void GetResult(object sender, EventArgs e)
+        {
+            var result = Calculate();
+            //label3.Text =$"{result}%";
+            SaveIntoFile(neadedBox.Text, readyBox.Text, Subject.Text);
+        }
+        private double ReturnTimeFromString(string text)
+        {
+            if (text.Contains(':'))
+            {
+                (double hours, double minutes) = GetHoursAndMinutes(':', text);
+                var result = hours * 60 + minutes;
+                return result;
+            }
+            if (text.Contains(' '))
+            {
+                (double hours, double minutes) = GetHoursAndMinutes(' ', text);
+                var result = hours * 60 + minutes;
+                return result;
+            }
+            return int.Parse(text);
+        }
+        private (double hours, double minutes) GetHoursAndMinutes(char symbol, string text)
+        {
+            var elements = text.Split(symbol).ToList();
+            var numbers = elements.Select(elem => double.Parse(elem)).ToArray();
+            return (numbers[0], numbers[1]);
+        }
+        private void SaveIntoFile(string value1, string value2, string subject)
+        {
+            using (StreamWriter writer = new StreamWriter("save.txt"))
+            {
+                var stringToSave = $"{value1},{value2},{subject}";
+                writer.WriteLine(stringToSave);
+            }
+            
+        }
+        private void ReadFromFile()
+        {
+            using (StreamReader reader = new StreamReader("save.txt"))
+            {
+               var result = reader.ReadLine();
+                var values = result.Split(',');
+                neadedBox.Text = values[0];
+                readyBox.Text = values[1];
+                Subject.Text = values[2];
+                Calculate();
+            }
+
+        }
+    }
+}
